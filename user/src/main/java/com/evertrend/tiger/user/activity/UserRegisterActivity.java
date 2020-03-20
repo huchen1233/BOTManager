d3a47be1f163a0b257318d931777f0a6d070c9ff
“TigerChen” <tiger@360yongli.com>
2020-03-20 16:58:13 +0800

    Initial commit

---
+++ user/src/main/java/com/evertrend/tiger/user/activity/UserRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.evertrend.tiger.user.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.evertrend.tiger.user.R;
+
+public class UserRegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yl_user_activity_register);
+    }
+}
