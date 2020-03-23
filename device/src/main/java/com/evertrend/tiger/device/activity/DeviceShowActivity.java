e5aa0309ba8a7dc8b3f3d50d78fdef0512dc061c
“TigerChen” <tiger@360yongli.com>
2020-03-23 21:37:11 +0800

    添加设备显示界面

---
+++ device/src/main/java/com/evertrend/tiger/device/activity/DeviceShowActivity.java
@@ -0,0 +1,17 @@
+package com.evertrend.tiger.device.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.evertrend.tiger.device.R;
+
+public class DeviceShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yl_device_activity_device_show);
+    }
+}
