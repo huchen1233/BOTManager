b9b6cfb5fd0694127b253df17fb032049818f58a
“TigerChen” <tiger@360yongli.com>
2020-03-23 20:35:18 +0800

    添加用户登录

---
+++ user/src/main/java/com/evertrend/tiger/user/activity/UserLoginActivity.java
@@ -0,0 +1,17 @@
+package com.evertrend.tiger.user.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.evertrend.tiger.user.R;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yl_user_activity_login);
+    }
+}
