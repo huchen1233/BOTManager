13e17a5cdcc365fa986b278e77c91533d4f3ebb1
“TigerChen” <tiger@360yongli.com>
2020-03-23 22:04:58 +0800

    app模块调用user和device模块

--- app/src/main/java/com/evertrend/tiger/evertrendbotmanager/activity/MainActivity.java
+++ app/src/main/java/com/evertrend/tiger/evertrendbotmanager/activity/MainActivity.java
@@ -1,17 +1,56 @@
 package com.evertrend.tiger.evertrendbotmanager.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.evertrend.tiger.device.activity.DeviceShowActivity;
 import com.evertrend.tiger.evertrendbotmanager.R;
+import com.evertrend.tiger.user.activity.UserLoginActivity;
+import com.evertrend.tiger.user.activity.UserRegisterActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_user_register;
+    private Button btn_user_login;
+    private Button btn_show_device;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btn_user_register = findViewById(R.id.btn_user_register);
+        btn_user_login = findViewById(R.id.btn_user_login);
+        btn_show_device = findViewById(R.id.btn_show_device);
+        btn_user_register.setOnClickListener(this);
+        btn_user_login.setOnClickListener(this);
+        btn_show_device.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_user_register:
+                openActivity(this, new UserRegisterActivity());
+                break;
+            case R.id.btn_user_login:
+                openActivity(this, new UserLoginActivity());
+                break;
+            case R.id.btn_show_device:
+                openActivity(this, new DeviceShowActivity());
+                break;
+        }
+    }
+
+    private void openActivity(MainActivity mainActivity, AppCompatActivity activity) {
+        Intent intent = new Intent(mainActivity, activity.getClass());
+        startActivity(intent);
     }
 }
