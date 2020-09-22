2bbda75a31abd7ef18897c5ae042e256c45f23b4
chenhu1233 <18930572454@qq.com>
2020-09-22 15:03:50 +0800

    更新是否为数字判断

--- common/src/main/java/com/evertrend/tiger/common/utils/general/Utils.java
+++ common/src/main/java/com/evertrend/tiger/common/utils/general/Utils.java
@@ -418,11 +418,6 @@ public class Utils {
      * @return
      */
     public static boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile("-?[0-9]+(\\\\.[0-9]+)?");
-        Matcher isNum = pattern.matcher(str);
-        if (!isNum.matches()) {
-            return false;
-        }
-        return true;
+        return str.matches("-?[0-9]+.*[0-9]*");
     }
 }
